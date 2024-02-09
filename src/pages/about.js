e828319ca5c5f7a7e183210f2e4e279097f966ae
hannah <113932134+hanmosh@users.noreply.github.com>
2024-02-09 13:22:44 -0800

    style: added colors, headers, and fonts to global.css

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,27 @@
 import Layout from "../components/layout";
 
 export default function AboutPage() {
-  return <Layout>About us</Layout>;
+  return (
+    <Layout>
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <br />
+        <p>Colors:</p>
+        <p style={{ color: "var(--eco-green)" }}> eco-green </p>
+        <p style={{ color: "var(--eco-light)" }}> eco-light </p>
+        <p style={{ color: "var(--eco-orange)" }}> eco-orange </p>
+        <br />
+        <p>Headers:</p>
+        <h1>Header 1</h1>
+        <h2>Header 2</h2>
+        <h3>Header 3</h3>
+        <h4>Header 4</h4>
+        <p>Body text</p>
+        <button className="btn-text">Button Text</button>
+      </div>
+    </Layout>
+  );
 }
