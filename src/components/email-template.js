da9965d88d95b85bebab8e82f65773c63fd4ffea
Aaron Ahmadyar <aaron.ahmadyar@gmail.com>
2024-02-09 12:46:39 -0800

    feat: endpoint configured to send email

---
+++ src/components/email-template.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const EmailTemplate = ({ firstName }) => (
+    <div>
+        <h1>Welcome, {firstName}!</h1>
+    </div>
+);
+
+export default EmailTemplate;
--- src/pages/api/forgot_password.js
+++ src/pages/api/forgot_password.js
@@ -1,17 +1,40 @@
-export default async function handler(req, res) {
-    if (req.method === "POST") {
-        try {
-            const { email } = req.body;
-            console.log(email);
+import EmailTemplate from "../../components/email-template";
+import { Resend } from "resend";
 
-            res.status(200).json({
-                message: "POST request received and processed successfully",
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export default async function handler(req, res) {
+    try {
+        //check method type
+        if (req.method === "POST") {
+            //extract email from request body
+            const { email } = req.body;
+
+            // Send email using Resend library
+            const { data, error } = await resend.emails.send({
+                from: "Acme <onboarding@resend.dev>",
+                to: [email],
+                subject: "Hello world",
+                react: EmailTemplate({ firstName: "John" }),
             });
-        } catch (error) {
-            console.error("Error:", error);
-            res.status(500).json({ error: "Internal server error" });
+
+            //error on send email
+            if (error) {
+                console.error("Error sending email:", error);
+                return res.status(400).json({ error: "Error sending email" });
+            }
+
+            console.log("Email sent successfully:", data);
+
+            //return status indicating successful email
+            res.status(200).json({
+                message: "Email sent successfully",
+            });
+        } else {
+            res.status(405).json({ error: "Method not allowed" });
         }
-    } else {
-        res.status(405).json({ error: "Method not allowed" });
+    } catch (error) {
+        console.error("Internal server error:", error);
+        res.status(500).json({ error: "Internal server error" });
     }
 }
