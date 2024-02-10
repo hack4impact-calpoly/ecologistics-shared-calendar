04491183b6abd5cfe638feb1a94da373d58add5e
Westluu <west1luu@gmail.com>
2024-02-10 00:43:07 -0800

    style: round btn, grid border along with long day names

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -4,6 +4,8 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import bootstrap5Plugin from '@fullcalendar/bootstrap5';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export default function CalendarPage() {
   return (
@@ -11,18 +13,22 @@ export default function CalendarPage() {
       <div className="calendar-container">
       <style>{calendarStyles}</style>
         <FullCalendar
+          themeSystem='bootstrap5'
           plugins={[
             resourceTimelinePlugin,
             dayGridPlugin,
             interactionPlugin,
             timeGridPlugin,
+            bootstrap5Plugin
           ]}
+
           customButtons={{
             AddEvent: {
               text: 'Add Event',
               click: function() {
                 alert("clicked");
               },
+              hint: 'none',
             }
           }}
 
@@ -31,7 +37,11 @@ export default function CalendarPage() {
             center: "prev title next",
             right: "AddEvent"
           }}
-          
+          buttonIcons={{
+            prev: 'bi-arrow-left', 
+            next: 'bi-arrow-right'
+
+          }}
           initialView="dayGridMonth"
           nowIndicator={true}
           editable={true}
@@ -42,8 +52,9 @@ export default function CalendarPage() {
               start: new Date(), 
               resourceId: "a",
               display: "auto"
-            },
+            }
           ]}
+          dayHeaderFormat={{ weekday: 'long' }}
         />
       </div>
     </Layout>
@@ -51,13 +62,30 @@ export default function CalendarPage() {
 }
 
 const calendarStyles = `
+  .fc .fc-prev-button, .fc .fc-next-button {
+    background-color: #335543;
+    border: none;
+    color: #FFF;
+    font-size: 1.5em;
+    border-radius: 1em; 
+    line-height: 1;
+  }
+
+  .fc .fc-prev-button:hover,
+  .fc .fc-next-button:hover,
+  .fc .fc-AddEvent-button:hover {
+    background-color: #eaeaea; 
+  }
+
   .fc-header-toolbar {
     display: flex;
     text-transform: uppercase;
+    padding-bottom: 1%;
   }
 
   .fc-toolbar-chunk:nth-child(2) {
     display: flex;
+    justify-content: space-between;
   }
 
   .fc-toolbar-chunk:last-child {
@@ -76,15 +104,16 @@ const calendarStyles = `
     padding: 0.5em 1em;
     border-color: #F7AB74;
     font-size: 1em;
+    border: none;
   }
 
   .fc .fc-daygrid-event-harness {
     max-width: 100%;
   }
 
-  .fc-event {
+  .fc .fc-event {
     background-color: #F7AB74;
-    border: none; 
+    border-color: #F7AB74;
     color: black;
     border-radius: 1em;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
@@ -98,6 +127,13 @@ const calendarStyles = `
 
   .fc-daygrid-event-dot {
     display: none;
+  }
+
+  .fc .fc-col-header-cell,
+  .fc .fc-daygrid-day,
+  .fc .fc-daygrid {
+    border: 1px solid #ddd;
+    border-right: 1px solid #ddd;
   }
 
 `;
