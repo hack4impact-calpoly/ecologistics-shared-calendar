34a7da6d682b777f2d1b50cb05de7c4a5e0cadfe
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2024-02-16 10:48:46 -0800

    feat: adds events with title, date, time information from user prompts, events disappear on refresh

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -4,9 +4,31 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import { useState } from "react";
+
+
+
 
 export default function CalendarPage() {
-  return (
+
+  const [events, setEvents] = useState([]);
+  const handleSelect = (info) => {
+    const eventNamePrompt = prompt("Enter event name");
+    const eventStart=prompt("Enter start time (hh:mm)");
+    var startTime = info.startStr.replace("00:00:00 GMT-0800 (Pacific Standard Time)","")
+    startTime=startTime+" "+eventStart
+    if (eventNamePrompt) {
+      setEvents([
+        ...events,
+        {
+          start: new Date(startTime),
+          title: eventNamePrompt,
+          id: Math.random().toString(),
+        },
+      ]);
+    }
+  };
+  return (    
     <Layout>
       <div className="calendar-container">
         <FullCalendar
@@ -24,6 +46,7 @@ export default function CalendarPage() {
           initialView="resourceTimelineWeek"
           nowIndicator={true}
           editable={true}
+          select={handleSelect}
           selectable={true}
           selectMirror={true}
           resources={[
@@ -33,9 +56,21 @@ export default function CalendarPage() {
           ]}
           initialEvents={[
             { title: "nice event", start: new Date(), resourceId: "a" },
+            
           ]}
+          events={events}
+          eventClick={
+            function(info){
+              alert('Event: ' + info.event.title+ '\nTime: '+info.event.start)
+            
+            }
+          }
+          
+          eventColor="#c293ff"
+         
         />
       </div>
     </Layout>
+    
   );
 }
