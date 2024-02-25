9dca2a59f1de7fdf157245665ec1ffb7d79a0ced
hannah <113932134+hanmosh@users.noreply.github.com>
2024-02-24 16:17:57 -0800

    about.tsx

---
+++ src/pages/about.tsx
@@ -0,0 +1,28 @@
+import Layout from "../components/layout";
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <Layout>
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <br />
+        <p>Colors:</p>
+        <p style={{ color: "var(--eco-green)" }}> eco-green </p>
+        <p style={{ color: "var(--eco-light)" }}> eco-light </p>
+        <p style={{ color: "var(--eco-orange)" }}> eco-orange </p>
+        <br />
+        <p>Headers:</p>
+        <h1>Header 1</h1>
+        <h2>Header 2</h2>
+        <h3>Header 3</h3>
+        <h4>Header 4</h4>
+        <p>Body text</p>
+        <button className="btn-text">Button Text</button>
+      </div>
+    </Layout>
+  );
+}
