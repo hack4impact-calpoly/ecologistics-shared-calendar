40dffac858194e18c2c7dc15238f1eae4970cb68
hannah <113932134+hanmosh@users.noreply.github.com>
2024-02-24 17:38:22 -0800

    nvm no admin folder

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Layout from "../../components/layout";
+import Layout from "../components/layout";
 import Image from "next/image";
 // placeholder profile image
 
