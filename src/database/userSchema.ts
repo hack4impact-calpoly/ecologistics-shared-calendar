c6e40713b3b65a0ed127b1bbc03ea26d73a8f09c
Devin Hadley <devinhadley4@gnauk.com>
2024-03-01 17:22:25 -0800

    feat: add event modal

--- src/database/userSchema.ts
+++ src/database/userSchema.ts
@@ -1,21 +1,10 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-// Interface for the User document
-interface UserDocument extends Document {
-  email: string;
-  password: string;
-  accountType: "user" | "admin"; // Account type: user or admin
-}
-
-// Schema for the User
-const UserSchema: Schema<UserDocument> = new Schema({
+//! Example user schema. Not guaranteed to work
+const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  accountType: { type: String, enum: ["user", "admin"], required: true }, // Specify account type
 });
 
-// Export the User model based on the schema
-const UserModel =
-  mongoose.models.Users || mongoose.model<UserDocument>("Users", UserSchema);
-
-export default UserModel;
+export default mongoose.models.Users ||
+  mongoose.model("Users", UserSchema, "Users");
