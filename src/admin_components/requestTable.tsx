1445c5dcf51ac38ef61baddbe0257d33e2c54cc9
jojayama <jodiyamane27@gmail.com>
2024-03-04 19:51:31 -0800

    feat: made it so there's a trash logo when the status changes from pending

--- src/admin_components/requestTable.tsx
+++ src/admin_components/requestTable.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Layout from "../components/layout";
+import { Delete, DeleteOutline} from "@mui/icons-material";
 
 const ITEMS_PER_PAGE = 11
 
@@ -50,68 +51,77 @@ export default function AdminPage() {
 
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Admin View</h1>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <table style={{ border: "1px solid black", borderCollapse: "collapse" }}>
-          <thead>
-            <tr>
-              <th style={{ padding: "10px" }}>Request For</th>
-              <th style={{ padding: "10px" }}>Email</th>
-              <th style={{ padding: "10px" }}>Status</th>
-              <th style={{ padding: "10px" }}>Date & Time</th>
-              <th style={{ padding: "10px"}}>Description</th>
+    <h1 style={{ textAlign: "center" }}>Admin View</h1>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <table style={{ border: "1px solid black", borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th style={{ padding: "10px" }}>Request For</th>
+            <th style={{ padding: "10px" }}>Email</th>
+            <th style={{ padding: "10px" }}>Status</th>
+            <th style={{ padding: "10px" }}>Date & Time</th>
+            <th style={{ padding: "10px"}}>Description</th>
+            <th style={{ padding: "10px"}}>Actions</th> {/* Add this column for buttons */}
+          </tr>
+        </thead>
+        <tbody>
+          {accountRequests
+          .slice(calculateRange().startIndex, calculateRange().endIndex)
+          .map((request) => (
+            <tr key={request.id} style={{ border: "1px solid black" }}>
+              <td style={{...styles.name, padding: "5px 50px" }}>{request.name}</td>
+              <td style={{...styles.email, padding: "5px 50px" }}>{request.email}</td>
+              <td style={{...styles.statusContainer, padding: "5px 50px" }}>
+                <span style={{color: request.status === 'Pending' ? "#0d4f90" : "#105631", background: request.status === 'Pending' ? "#d4eaff" : "#E0F5EC", padding: "5px 10px", borderRadius: "20px", fontWeight: "700px"}}>
+                  {request.status}
+                </span>
+              </td>
+              <td style={{ padding: "5px 50px" }}>
+                <span style={{ ...styles.date, fontSize: "16px", color: "black", fontWeight: "700", display: "block"}}>
+                  {request.date}
+                </span>
+                <span  style={{ ...styles.time, fontSize: "14px", color: "black", display: "block"}}>
+                  {request.time}
+                </span>
+              </td>
+              <td style = {{...styles.description,padding: "5px 50px"}}>{request.description}</td>
+              <td style={{...styles.buttonContainer, padding: "20px 50px"}}>
+                {request.status === 'Pending' ? ( // Render buttons based on status
+                  <>
+                    <button
+                      style={{...styles.buttons, padding: "8px 15px"}}
+                      onClick={() => handleAction(request.id.toString(), "accepted")}
+                      onMouseOver={(e: React.MouseEvent<HTMLButtonElement>) => ((e.target as HTMLButtonElement).style.backgroundColor = "#e69153")}
+                      onMouseOut={(e: React.MouseEvent<HTMLButtonElement>) => ((e.target as HTMLButtonElement).style.backgroundColor = "#f7ab74")}
+                    >
+                      Approve
+                    </button>
+                    <button
+                      style={{...styles.buttons, padding: "8px 25px", background: "#d9d9d9"}}
+                      onClick={() => handleAction(request.id.toString(), "declined")}
+                      onMouseOver={(e: React.MouseEvent<HTMLButtonElement>) => ((e.target as HTMLButtonElement).style.backgroundColor = "#C6C6C6")}
+                      onMouseOut={(e: React.MouseEvent<HTMLButtonElement>) => ((e.target as HTMLButtonElement).style.backgroundColor = "#D9D9D9")}
+                    >
+                      Deny
+                    </button>
+                  </>
+                ) : ( 
+                  // Change Approve/Deny to trash can icon when status is "Accept"
+                  <button
+                    onClick={() => handleAction(request.id.toString(), "trash")}
+                    onMouseOver={(e) => { e.currentTarget.style.backgroundColor = "#FECACC"; }}
+                    onMouseOut={(e) => { e.currentTarget.style.backgroundColor = "#F6E0E1"; }}
+                    style={{ padding: 0, background: "#f6e0e1", border: "none", cursor: "pointer", borderRadius: "100px", color: "#9C2C30"}}
+                  >
+                    <DeleteOutline style={{ fontSize: 38, backgroundColor: "transparent", borderRadius: "100px", padding: "10px" }} />
+                  </button>
+                )}
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {accountRequests
-            .slice(calculateRange().startIndex, calculateRange().endIndex)
-            .map((request) => (
-              <tr key={request.id} style={{ border: "1px solid black" }}>
-                {/* <td style={{ padding: "10px" }}>
-                  <Image
-                    src={request.image}
-                    width={50}
-                    height={50}
-                    alt="profile-image"
-                    placeholder="empty"
-                    style={{ borderRadius: "50%" }}
-                  />
-                </td> */}
-                <td style={{...styles.name, padding: "5px 50px" }}>{request.name}</td>
-                <td style={{...styles.email, padding: "5px 50px" }}>{request.email}</td>
-                <td style={{...styles.statusContainer, padding: "5px 50px" }}><span style={{color: request.status === 'Pending' ? "#0d4f90" : "#105631", background: request.status === 'Pending' ? "#d4eaff" : "#E0F5EC", padding: "5px 10px", borderRadius: "20px", fontWeight: "700px"}}>{request.status}</span></td>
-                <td style={{ padding: "5px 50px" }}>
-                  <span style={{ ...styles.date, fontSize: "16px", color: "black", fontWeight: "700", display: "block"}}>
-                    {request.date}
-                  </span>
-                  <span style={{ ...styles.time, fontSize: "14px", color: "black", display: "block"}}>
-                    {request.time}
-                  </span>
-                </td>
-                <td style = {{...styles.description,padding: "5px 50px"}}>{request.description}</td>
-                <td style={{...styles.buttonContainer, padding: "20px 50px"}}>
-                  <button
-                    style={{...styles.buttons, padding: "8px 15px"}}
-                    onClick={() => handleAction(request.id.toString(), "accepted")}
-                    onMouseOver={(e: React.MouseEvent<HTMLButtonElement>) => ((e.target as HTMLButtonElement).style.backgroundColor = "#e69153")}
-                    onMouseOut={(e: React.MouseEvent<HTMLButtonElement>) => ((e.target as HTMLButtonElement).style.backgroundColor = "#f7ab74")}
-                  >
-                    Approve
-                  </button>
-                  <button
-                    style={{...styles.buttons, padding: "8px 25px", background: "#d9d9d9"}}
-                    onClick={() => handleAction(request.id.toString(), "declined")}
-                    onMouseOver={(e: React.MouseEvent<HTMLButtonElement>) => ((e.target as HTMLButtonElement).style.backgroundColor = "#e69153")}
-                    onMouseOut={(e: React.MouseEvent<HTMLButtonElement>) => ((e.target as HTMLButtonElement).style.backgroundColor = "#f7ab74")}
-                  >
-                    Deny
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
+    </div>
       <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
         {Array.from({ length: Math.ceil(accountRequests.length / ITEMS_PER_PAGE) }, (_, index) => (
           <button
@@ -193,4 +203,15 @@ const styles : { [key: string]: React.CSSProperties } = {
     height: "50%",
     width: "50%",
   },
+  icon:{
+    backgroundColor: "transparent",
+  },
+  trashButton:{
+    background: "#F6E0E1",
+    color: "#9C2C30",
+    border: "0px",
+    cursor: "pointer",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 };
