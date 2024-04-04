315c57631f32565d018a2dc8bce052eed5497618
Aaron Ahmadyar <aaron.ahmadyar@gmail.com>
2024-04-04 14:42:32 -0700

    feat: added clerk auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "../styles/globals.css";
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <ClerkProvider>
+            <Component {...pageProps} />;
+        </ClerkProvider>
+    );
 }
