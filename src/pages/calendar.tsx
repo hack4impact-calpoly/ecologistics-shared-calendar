14829d79803ea0632b6ec1c769c4bf4abd420d7f
Matthew <mattbosio08@gmail.com>
2024-04-30 17:22:57 -0700

    style: redid verification page for signup

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -128,7 +128,6 @@ export default function CalendarPage() {
       <div className={style1.calendarPageContainer}>
         <div className="calendar-container">
           <div style={styles.signoutContainer}>
-
             <button
               onClick={handleLogout}
               onMouseOver={(e) =>
@@ -144,7 +143,7 @@ export default function CalendarPage() {
               Logout
             </button>
             <Link prefetch={false} href="/adminEvents">
-             <button
+              <button
                 onMouseOver={(e) =>
                   ((e.target as HTMLButtonElement).style.backgroundColor =
                     "#e69153")
